3fb930991e3b97776a240dd25ac2455a901a0478
Mark Spratt <panzerlink@gmail.com>
2022-09-01 18:58:41 -0500

    add darkmode. This is just a start. Still need to define pages css to use it.

---
+++ public/js/darkmode.js
@@ -0,0 +1,13 @@
+
+const switcher = document.querySelector('#theme-switcher'); // select the switcher
+
+switcher.addEventListener('input', e =>
+setTheme(e.target.value))
+// set the theme on input and save to local storage
+
+const setTheme = theme => {
+    document.documentElement.setAttribute('color-scheme', theme);
+    localStorage.setItem('color-scheme', theme);
+}
+// set the theme on load
+setTheme(localStorage.getItem('color-scheme') || 'auto');
