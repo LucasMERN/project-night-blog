49fd900fc1f3089be9bb04793f372f3ad073f815
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-01 20:50:54 -0400

    added DB connection

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const { application } = require("express");
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const connectDB = require('./config/database');
+
+require('dotenv').config({path: './config/.env'});
+
+connectDB();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let db,
-    dbConnectionStr,
-    dbName;
+// let db,
+//     dbConnectionStr,
+//     dbName;
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`);
-        db = client.db(dbName);
-    });
+// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+//     .then(client => {
+//         console.log(`Connected to ${dbName} Database`);
+//         db = client.db(dbName);
+//     });
 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on port ${process.env.PORT}`);
 });
