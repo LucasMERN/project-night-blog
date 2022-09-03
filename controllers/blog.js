4e6066a6ca4f181608a1ef9c18dde754604d8316
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-03 11:40:40 -0400

    added markdown and edited models/controllers

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,5 +1,24 @@
+const Blog = require('../models/Blog')
+
 module.exports = {
     getIndex: (req,res)=>{
-        res.render('blog.ejs')
+        res.render('blog')
+    },
+    newBlogPage: (req, res)=>{
+        res.render('new.ejs')
+    },
+    newBlogPost: async(req, res)=>{
+        try {
+            await Blog.create({
+                title: req.body.title,
+                intro: req.body.intro,
+                markdown: req.body.markdown,
+                totalLikes: 0,
+                totalComments: 0,
+            })
+            res.redirect('/')
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
--- models/Blog.js
+++ models/Blog.js
@@ -1,5 +1,16 @@
 // model for blog posts
 const mongoose = require('mongoose');
+//marked allows us to use markdown
+const { marked } = require('marked');
+//slugify allows us to convert our id into a url friendly slug, looks way better 
+const slugify = require('slugify');
+// This returns a function that sanitizes our HTML so malicious code cannot be added into our markdown
+const createDOMPurify = require('dompurify');
+// We destructure because we only want the JSDOM portion of what this returns
+const { JSDOM } = require('jsdom')
+const window = new JSDOM('').window
+const DOMPurify = createDOMPurify(window)
+// We use schemas to structure our data
 const Schema = mongoose.Schema;
 
 const BlogSchema = new Schema({
@@ -7,14 +18,45 @@ const BlogSchema = new Schema({
         type: String,
         required: true
     },
-    body: {
+    intro: {
+        type: String,
+        required: true
+    },
+    markdown: {
         type: String,
         required: true
     },
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    totalLikes: {
+        type: Number
+    },
+    totalComments: {
+        type: Number
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true
     }
 });
 
-const Blog = mongoose.model('Blog', BlogSchema);
+//Set validations and before attributes (pre validate)
+BlogSchema.pre('validate', function(next){
+    if(this.title){
+        //lower:true sets our slug to lowercase, string will remove weird chars like colons
+        this.slug = slugify(this.title, {lower: true, strict: true})
+    }
+    if(this.markdown){
+        this.sanitizedHtml = DOMPurify.sanitize(marked.parse(this.markdown))
+    }
+    next()
+})
+
+module.exports = mongoose.model('Blog', BlogSchema);
