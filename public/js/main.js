a3b7c5c971da1845260634492c05271841e3bf4b
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-10 20:56:51 -0400

    added show pw feature that allows you to toggle between password and text, also added eyeball toggle

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+const togglePassword = document.querySelector('#togglePassword')
+const togglePassword2 = document.querySelector('#togglePassword2')
+const password = document.querySelector('#password')
+const password2 = document.querySelector('#password2')
+
+// Make sure password id is not null. If not, toggle visibility of password with 'password' id, toggle eye icon as well
+if(togglePassword){
+    togglePassword.addEventListener('click', toggleFirstPw)
+}
+function toggleFirstPw (){
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password'
+    password.setAttribute('type', type)
+    this.classList.toggle('fa-eye')
+}
+
+// Make sure password2 id is not null. If not, toggle visibility of password with 'password2' id, toggle eye icon as well
+if(togglePassword2){
+    togglePassword2.addEventListener('click', toggleSecondPw)
+}
+
+function toggleSecondPw (){
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password'
+    password2.setAttribute('type', type)
+    this.classList.toggle('fa-eye')
+}
