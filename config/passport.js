bcffb0184e378f437f53168a577522abb301d13a
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-11 14:50:10 -0400

    installed fb dependency, routed fb req and fb callback

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook');
 const mongoose = require('mongoose')
 const User = require('../models/User')
 
@@ -26,5 +27,41 @@ module.exports = function (passport) {
 
   passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => done(err, user))
+  }),
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:2002/login/facebook/callback"
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    console.log(profile)
+    // const newUser = {
+    //   username: profile.displayName,
+    //   email: profile.email,
+    //   password: 'N/A',
+    // }
+    // try {
+    //   let user = await User.findOne({ email: profile.email })
+
+    //   if (user) {
+    //     done(null, user)
+    //   } else {
+    //     user = await User.create(newUser)
+    //     done(null, user)
+    //   }
+    // } catch (error) {
+    //   console.log(error)
+    // }
+  }
+));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
   })
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user))
+  })
+
 }
