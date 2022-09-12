0abbe7be9a66e4de2ff2c64da5c3659eb8997282
Lucas Winkler <winkler.lucas1995@gmail.com>
2022-09-11 20:35:45 -0500

    added password valiation for signup

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+const { serialize } = require("bson")
+
+const togglePassword = document.querySelector('#togglePassword')
+const togglePassword2 = document.querySelector('#togglePassword2')
+const password = document.querySelector('#password')
+const password2 = document.querySelector('#password2')
+
+// Make sure password id is not null. If not, toggle visibility of password with 'password' id, toggle eye icon as well
+if(togglePassword){
+    togglePassword.addEventListener('click', toggleFirstPw)
+}
+function toggleFirstPw (){
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password'
+    password.setAttribute('type', type)
+    this.classList.toggle('fa-eye')
+}
+
+// Make sure password2 id is not null. If not, toggle visibility of password with 'password2' id, toggle eye icon as well
+if(togglePassword2){
+    togglePassword2.addEventListener('click', toggleSecondPw)
+}
+
+function toggleSecondPw (){
+    const type = password2.getAttribute('type') === 'password' ? 'text' : 'password'
+    password2.setAttribute('type', type)
+    this.classList.toggle('fa-eye')
+}
+
+function pwValidation() {
+
+    let str = document.getElementById('password').value;
+    let message = document.getElementById('passwordError');
+    
+    if (!str.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/)) {
+        message.innerText = 'Passwords must be between 8-30 characters long and contain atleast one: uppercase letter, lowercase letter, special character, & number.';
+    } else {
+        message.innerText = '';
+    }
+
+  };
+
+  function pwMatch() {
+    if(document.getElementById('password').value !== document.getElementById('password2').value){
+        document.getElementById('passwordMatch').innerText = 'Passwords must match.';
+    } else {
+        document.getElementById('passwordMatch').innerText = '';
+    }
+  }
