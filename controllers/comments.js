49c7cdeda1ea82ffeb79bbf5b4852af0b750d161
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-12 21:12:38 -0400

    created model and controller to send comment to our db with unique identifier which will be used for filtering

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,5 +1,17 @@
+const Comment = require('../models/Comment')
+const Blog = require('../models/Blog')
+
 module.exports = {
     addComment: async (req, res) => {
-        console.log('hey')
+        try {
+            await Comment.create({
+                userName: req.user.userName,
+                comment: req.body.comment,
+                email: req.body.title
+            })
+            res.redirect('/')
+        } catch (error) {
+            console.log(error)
+        }
     }
 }
