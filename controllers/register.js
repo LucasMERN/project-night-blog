fbf970a479901317cb127ef2f8055a5d4bb04cfa
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-15 20:33:02 -0400

    adjusted register controller to prompt user if username is already taken

--- controllers/register.js
+++ controllers/register.js
@@ -5,8 +5,9 @@ module.exports = {
         res.render('sign-up.ejs')
     },
     createUser: async (req, res) => {
-        const { email, password, password2 } = req.body
+        const { email, username, password, password2 } = req.body
         const exists = await User.findOne({email: email});
+        const usernameExists = await User.findOne({username: username});
         let errors = [];
         //Check fields
         if(!email || !password || !password2){
@@ -17,6 +18,8 @@ module.exports = {
             errors.push({msg: 'Password must be at least 8 characters long.'});
         }if(exists){
             errors.push({msg: 'This email is already taken.'});
+        }if(usernameExists){
+            errors.push({msg: 'Username already taken. Please try again.'})
         }if(!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/)){
             errors.push({msg: 'Passwords must be between 8-30 characters long and contain at least one: uppercase letter, lowercase letter, special character, & number.'})
         }if(errors.length > 0){
