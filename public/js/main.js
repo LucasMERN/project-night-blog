eb7d3518f6180ecf06071ea43248815747327c2c
Lucas Winkler <winkler.lucas1995@gmail.com>
2022-09-17 15:52:17 -0500

    fixed button cursor

--- public/js/main.js
+++ public/js/main.js
@@ -33,6 +33,7 @@ function pwValidation() {
         message.innerText = 'Passwords must be between 8-30 characters long and contain atleast one: uppercase letter, lowercase letter, special character, & number.';
         document.getElementById('signUp-btn').style.color = "rgba(255, 255, 255, 0.38)"
         document.getElementById('signUp-btn').style.backgroundColor = "rgba(14, 16, 27, 0.38)"
+        document.getElementById('signUp-btn').style.cursor = 'not-allowed';
     } else {
         message.innerText = '';
     }
@@ -46,6 +47,7 @@ function pwValidation() {
         document.getElementById('passwordMatch').innerText = '';
         document.getElementById('signUp-btn').style.color = "rgb(255, 255, 255)"
         document.getElementById('signUp-btn').style.backgroundColor = "rgb(14, 16, 27)"
+        document.getElementById('signUp-btn').style.cursor = 'pointer';
     }
   }
 
@@ -53,10 +55,12 @@ function inputDisable() {
   if(document.getElementById('text').value.length > 0 && document.getElementById('password').value == document.getElementById('password2').value && document.getElementById('password').value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/)) {
     document.getElementById('signUp-btn').style.color = "rgb(255, 255, 255)"
     document.getElementById('signUp-btn').style.backgroundColor = "rgb(14, 16, 27)"
+    document.getElementById('signUp-btn').style.cursor = 'pointer';
   } else {
     if(document.getElementById('text').value.length < 1 || document.getElementById('email').value.length < 1) {
         document.getElementById('signUp-btn').style.color = "rgba(255, 255, 255, 0.38)"
         document.getElementById('signUp-btn').style.backgroundColor = "rgba(14, 16, 27, 0.38)"
+        document.getElementById('signUp-btn').style.cursor = 'not-allowed';
     }
   }
 };
@@ -65,5 +69,6 @@ function loginButton() {
     if(document.getElementById('email').value.length > 0 && document.getElementById('password').value.length > 0) {
         document.getElementById('logIn-btn').style.color = "rgb(255, 255, 255)"
     document.getElementById('logIn-btn').style.backgroundColor = "rgb(14, 16, 27)"
+    document.getElementById('logIn-btn').style.cursor = 'pointer';
     }
 }
