51bdf9dfc45127d34816f190d7198de6a64d9211
misko9251 <101610922+misko9251@users.noreply.github.com>
2022-09-20 20:24:07 -0400

    wrote form and logic to update db when writing profile bio

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,22 @@
+const User = require('../models/User')
+
 module.exports = {
     getProfile: async (req, res) => {
         try {
             res.render('index.ejs', {routeName: 'profile', user: req.user})
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    postBio: async (req, res) => {
+        try {
+            await User.findOneAndUpdate(
+                {_id: req.body.user},
+                {
+                    $set: {bio: req.body.bio}
+                }
+            );
+            res.send('works')
         } catch (error) {
             console.log(error)
         }
--- routes/profile.js
+++ routes/profile.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const profileController = require('../controllers/profile');
 
 router.get('/:id', profileController.getProfile);
+router.put('/:id/updateBio', profileController.postBio);
 
 module.exports = router
