772b110c1473c183c5959af7e9721dbf9a396b5c
Matthew Miskovsky <matt.miskovsky@gmail.com>
2022-12-16 22:34:29 -0500

    fixed bug in likesController, was looking for key id instead of key _id which was causing a db bug

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -4,22 +4,22 @@ const User = require('../models/UserSchema')
 module.exports = {
     updateLike: async (req, res) => {
         try {
-            //Find the blog we are currently viewing and populate its author field
-            const blog = await Blog.findOne({_id: req.params.id}).populate('author');
-            //Check if the logged in user has liked the current blog
-            const liked = await User.findOne({_id: req.user.id, likes: {$in: [blog._id]}});
+            const user = await User.findOne({_id: req.user.id});
+            const liked = user.likes.includes(req.params.id);
+            console.log(liked)
+            console.log(req.params.id)
             if(liked){
                 await User.updateOne({_id: req.user.id}, {
                     $pull: {likes: req.params.id}
                 })
-                await Blog.updateOne({id: req.params.id}, {
+                await Blog.updateOne({_id: req.params.id}, {
                     $pull: {likedBy: req.user.id}
                 })
             }else{
                 await User.updateOne({_id: req.user.id}, {
                     $push: {likes: req.params.id}
                 })
-                await Blog.updateOne({id: req.params.id}, {
+                await Blog.updateOne({_id: req.params.id}, {
                     $push: {likedBy: req.user.id}
                 })
             }
