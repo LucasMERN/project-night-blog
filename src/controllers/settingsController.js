afa95b987a89b9753b805676aea0772c949c48e5
Lucas Winkler <winkler.lucas1995@gmail.com>
2023-01-04 19:46:02 -0600

    added styling based on the route a user is on for better visual navigation.

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -3,7 +3,7 @@ const User = require('../models/UserSchema')
 module.exports = {
     getSettings: async (req, res) => {
         try {
-            res.render('settings.ejs')
+            res.render('settings.ejs', {routeName: 'settings', user: req.user})
         } catch (error) {
             console.log(error)
         }
