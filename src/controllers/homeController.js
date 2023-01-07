56b92a466a5d86a847e3b640ee0197b797c4fb80
Lucas Winkler <winkler.lucas1995@gmail.com>
2023-01-07 16:23:38 -0600

    added specific users to the home controller to populate the people to follow tab in discovery ejs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,15 @@
 const Blog = require('../models/BlogSchema')
+const User = require('../models/UserSchema')
 
 module.exports = {
     getIndex: async (req,res)=>{
         try {
             // find all of our blogs, sort them in descending order so the newest ones are on top
             const blogs = await Blog.find().sort({ createdAt: -1 }).populate('author').exec()
-            res.render('mainLayout.ejs', {blogs: blogs, user: req.user, routeName: 'home'})
+            const ObjectId = require('mongoose').Types.ObjectId;
+            const userId = new ObjectId('639e48f1e551d5ac769fbe20');
+            const specificUser = await User.findOne({ _id: userId });
+            res.render('mainLayout.ejs', {blogs: blogs, user: req.user, specificUser: specificUser, routeName: 'home'})
         } catch (error) {
             console.log(error)
         }
