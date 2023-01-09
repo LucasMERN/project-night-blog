36608b7c2b0e9518aebd1a06352dba1f17563801
Lucas Winkler <winkler.lucas1995@gmail.com>
2023-01-09 16:22:02 -0600

    settings update functionality is validated on both front and backend

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -35,42 +35,50 @@ module.exports = {
 
     updateUsername: async (req, res) => {
         try {
-            await User.findOneAndUpdate({_id: req.user.id},
-                {
-                userName: req.body.userName
-                },
-                console.log(req.body.userName)
-            );
-            res.redirect('back')
+            // Check if the username is already in the database
+            const usernameExists = await User.exists({ userName: req.body.username });
+            if (usernameExists) {
+                res.render('settings.ejs', { user: req.user, routeName: 'settings' });
+                return;
+            }
+            // If the username is not in the database, update the user's username
+            await User.findOneAndUpdate({ _id: req.user.id }, { userName: req.body.userName });
+            res.redirect('back');
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     },
 
     updateEmail: async (req, res) => {
         try {
-            await User.findOneAndUpdate({_id: req.user.id},
-                {
-                email: req.body.email
-                }
-            );
-            res.redirect('back')
+            // Check if the email is already in the database
+            const emailExists = await User.exists({ email: req.body.email });
+            if (emailExists) {
+                res.render('settings.ejs', { user: req.user, routeName: 'settings' });
+                return;
+            }
+            // If the email is not in the database, update the user's email
+            await User.findOneAndUpdate({ _id: req.user.id }, { email: req.body.email });
+            res.redirect('back');
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     },
 
     updatePassword: async (req, res) => {
         try {
+            // Check if the password contains at least one uppercase letter, lowercase letter, special character, and number
+            if(!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/)){
+                res.render('settings.ejs', { user: req.user, routeName: 'settings' });
+                return;
+            }
+            // If the password adheres to the specified parameters, hash the password and update the user's password
             const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            await User.findOneAndUpdate({_id: req.user.id},
-                {
-                password: hashedPassword
-                }
-            );
-            res.redirect('back')
+            await User.findOneAndUpdate({ _id: req.user.id }, { password: hashedPassword });
+            res.redirect('back');
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
+      
 }
