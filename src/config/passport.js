51a072c20f8f15914f5f6e9ea3ce32f6db4a2f32
Lucas Winkler <94091522+LucasMERN@users.noreply.github.com>
2023-01-13 09:40:36 -0600

    Got rid of Facebook and Twitter auth

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,8 +1,6 @@
 const LocalStrategy = require('passport-local').Strategy
-const FacebookStrategy = require('passport-facebook');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
-// const TwitterStrategy = require('passport-twitter').Strategy;
 const mongoose = require('mongoose')
 const User = require('../models/UserSchema')
 
@@ -31,34 +29,6 @@ module.exports = function (passport) {
   passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => done(err, user))
   }),
-
-//   passport.use(new FacebookStrategy({
-//     clientID: process.env.FACEBOOK_APP_ID,
-//     clientSecret: process.env.FACEBOOK_APP_SECRET,
-//     callbackURL: "http://localhost:2002/login/facebook/callback"
-//   },
-//   async (accessToken, refreshToken, profile, done) => {
-//     console.log(profile)
-//     const newUser = {
-//       userName: profile.displayName,
-//       // We will be filtering our database by email address which must be unique, therefore we will assign the unique profile id to our email property
-//       email: profile.id,
-//       password: 'N/A',
-//     }
-//     try {
-//       let user = await User.findOne({ email: profile.id })
-
-//       if (user) {
-//         done(null, user)
-//       } else {
-//         user = await User.create(newUser)
-//         done(null, user)
-//       }
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }
-// ));
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
@@ -130,34 +100,6 @@ async (accessToken, refreshToken, profile, done) => {
   }
 }
 ));
-
-// passport.use(new TwitterStrategy({
-//   consumerKey: process.env.TWITTER_CONSUMER_KEY,
-//   consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-//   callbackURL: "http://localhost:2002/login/twitter/callback"
-// },
-// async (accessToken, refreshToken, profile, done) => {
-//   console.log(profile)
-//   const newUser = {
-//     userName: profile.displayName,
-//     // We will be filtering our database by email address which must be unique, therefore we will assign the unique profile id to our email property
-//     email: profile.id,
-//     password: 'N/A',
-//   }
-// try {
-//   let user = await User.findOne({ email: profile.id })
-
-//   if (user) {
-//     done(null, user)
-//   } else {
-//     user = await User.create(newUser)
-//     done(null, user)
-//   }
-// } catch (error) {
-//   console.log(error)
-// }
-// }
-// ));
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
